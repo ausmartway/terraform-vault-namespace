557806f45ca277ff38eb3decb6acbcc9435834ff
Yulei Liu <yulei.liu@gmail.com>
2021-09-01 21:43:29 +1000

    add description to outputs

--- output.tf
+++ output.tf
@@ -1,8 +1,10 @@
 output "vault-namespace" {
+  description = "The full path of the namespace created."
   value = vault_namespace.namespace.path
 }
 
 output "vault-token" {
+  description = "The Vault token created for managing the namespace."
   value = vault_token.namespace-admin-token.client_token
   sensitive = true
 }
